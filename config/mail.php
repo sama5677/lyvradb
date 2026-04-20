756e7f7f036bb271fa2f2d6486926e066707ce2f
sama5677 <sm4786400@gmail.com>
2026-04-21 00:58:12 +0200

    update on the php.mail

--- config/mail.php
+++ config/mail.php
@@ -38,16 +38,16 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-        ],
+    'transport' => 'smtp',
+    'scheme' => env('MAIL_SCHEME'),
+    'url' => env('MAIL_URL'),
+    'host' => env('MAIL_HOST', '127.0.0.1'),
+    'port' => env('MAIL_PORT', 2525),
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
+    'timeout' => 15,                    // ← ADD THIS (prevents 1.5 min hang)
+    'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+],
 
         'ses' => [
             'transport' => 'ses',
